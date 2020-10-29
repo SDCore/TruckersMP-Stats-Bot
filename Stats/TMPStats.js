4e57dd2b52c08b2fc6d6b5be7d53b6ec883c4f35
SDCore <sdcored@gmail.com>
2020-10-29 01:36:48 -0500

    Running Bot... but no optimizations :( (soon)

---
+++ Stats/TMPStats.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const { ShardingManager } = require("discord.js");
+const manager = new ShardingManager("./bot.js", {
+  token: process.env.DISCORD_TOKEN,
+});
+
+manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
--- getAPIData.js
+++ getAPIData.js
@@ -1,1 +1,45 @@
-getAPIData.js;
+const fs = require("fs");
+const axios = require("axios"); // Load http server for communicating with the API
+const moment = require("moment");
+
+function pingPlayerCount() {
+  // Ping the API for server data
+  console.log("Pinging API, please wait...");
+  axios
+    .get("https://api.truckersmp.com/v2/servers")
+    .then((res) => {
+      // If the response is valid
+      if (res.data) {
+        var jsonresponse = JSON.stringify(res.data);
+        // Write data in 'Output.txt' .
+        fs.writeFile("apiOutput.json", jsonresponse, (err) => {
+          // In case of a error throw err.
+          if (err) throw err;
+        });
+        console.log(`[${moment().format("hh:mm:s")}] Logged json response`);
+      } else {
+        console.log("Could not fetch data from API, the server may be down.");
+      }
+    })
+    .catch(
+      (err) =>
+        console.log(
+          "Error pinging the API for data: ",
+          err,
+          " Sorry! Bots should be set to DND shortly."
+        ),
+      fs.writeFile("apiOutput.json", '{"error" : "true"}', (err) => {
+        if (err) throw err;
+      })
+    );
+}
+
+pingPlayerCount();
+
+setInterval(function () {
+  var date = new Date();
+
+  if (date.getMinutes() % 2.5 == 0) {
+    pingPlayerCount();
+  }
+}, Math.max(1, 1) * 60 * 1000);
