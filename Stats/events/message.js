1d1e007d96afb3343b89e14483ae146ad10cab5d
SDCore <sdcored@gmail.com>
2020-10-31 04:27:07 -0500

    Added Message Event Handler
    - Message event handler now exists
    - Includes basic implementation of command handler
    - Handles being DM'd; replies with a link to the support server and alerts the support server of a DM.

---
+++ Stats/events/message.js
@@ -0,0 +1,38 @@
+const { client, Discord } = require("../TMPStats.js");
+const config = require("../config.json");
+const moment = require("moment");
+
+const fs = require("fs");
+
+const prefix = config.prefix;
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`../commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on("message", (message) => {
+  // If the author of the message is the bot, ignore it
+  if (message.author.equals(client.user)) return;
+
+  if (message.channel.type == "dm") {
+    dmContent = `**(${moment(message.createdAt).format(
+      "MMMM Do, YYYY h:mm:ss A"
+    )}) [${message.author}]**\n${message.content}`;
+
+    client.guilds.cache
+      .get("664717517666910220")
+      .channels.cache.get("772025716073824266")
+      .send(dmContent);
+    message.reply(
+      "Hey! Join the support server at https://discord.gg/f3Pa8vJ to get support.\n\n*These DM's are not monitored.*"
+    );
+    return;
+  }
+});
