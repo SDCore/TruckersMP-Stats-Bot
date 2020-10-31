f47cf4eb70937ff98611713685d1d5b2b7d92875
SDCore <sdcored@gmail.com>
2020-10-31 04:13:33 -0500

    Set presence on initial bot load before updating it once every 5 minutes

--- Stats/events/ready.js
+++ Stats/events/ready.js
@@ -1,22 +1,33 @@
 const { client } = require("../TMPStats.js");
-const config = require("../config.json");
 const moment = require("moment");
 
 // Runs when the client connects to Discord
 client.once("ready", () => {
   console.log(`Logging in as ${client.user.tag}`);
 
-  // Set the bots presence to inform the user how to use the bot
-  setInterval(() => {
+  function setPresence() {
     client.user
       .setPresence({
         activity: {
-          name: `!!help for help | Providing data for ${client.guilds.cache.size} servers`,
+          name: `!!help | Providing data for ${client.guilds.cache.size} servers`,
           type: "WATCHING",
         },
         status: "online",
       })
       .catch(console.error);
+  }
+
+  // Set bot presence on initial load
+  // This will prevent the presence from
+  // being blank for the first 5 minutes
+  // the bot runs
+  setPresence();
+  console.log(`[${moment().format("hh:mm:ss")}] Updated presence for bot`);
+
+  // Set the bots presence and updates
+  // it every 5 minutes
+  setInterval(() => {
+    setPresence();
     console.log(`[${moment().format("hh:mm:ss")}] Updated presence for bot`);
   }, 1000 * 60 * 5);
 });
