866b98ffc885e94ca62c220c3b93051296e2013e
SDCore <sdcored@gmail.com>
2020-10-30 23:46:06 -0500

    Added Guild Count to Bot Presence

--- Stats/events/ready.js
+++ Stats/events/ready.js
@@ -1,15 +1,22 @@
 const { client } = require("../TMPStats.js");
 const config = require("../config.json");
+const moment = require("moment");
 
 // Runs when the client connects to Discord
 client.once("ready", () => {
   console.log(`Logging in as ${client.user.tag}`);
 
   // Set the bots presence to inform the user how to use the bot
-  client.user
-    .setPresence({
-      activity: { name: "!!help for help", type: "WATCHING" },
-      status: "online",
-    })
-    .catch(console.error);
+  setInterval(() => {
+    client.user
+      .setPresence({
+        activity: {
+          name: `!!help for help | Providing data for ${client.guilds.cache.size} servers`,
+          type: "WATCHING",
+        },
+        status: "online",
+      })
+      .catch(console.error);
+    console.log(`[${moment().format("hh:mm:ss")}] Updated presence for bot`);
+  }, 1000 * 60 * 5);
 });
